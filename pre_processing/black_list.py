e95d6f4a2a3fc780b59b313df772f789b29fc2e3
noamsh <noam.shapi@gmail.com>
2023-08-07 16:34:12 +0300

    started working on scvi need to run on GPU

--- pre_processing/black_list.py
+++ pre_processing/black_list.py
@@ -5,6 +5,6 @@ import anndata as ad
 
 
 def drop_blacklist_genes_by_prefix(adata: ad.AnnData, blacklist_genes_prefixes: List[str]) -> ad.AnnData:
-    blacklist_genes_regexs = [f"^{prefix}*" for prefix in blacklist_genes_prefixes]
+    blacklist_genes_regexs = [f"^{re.escape(prefix)}*" for prefix in blacklist_genes_prefixes]
     adata = adata[:, [gn for gn in adata.var_names if re.match("|".join(blacklist_genes_regexs), gn)]]
     return adata
