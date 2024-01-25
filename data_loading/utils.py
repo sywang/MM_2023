9103a5078258775ea5027f60b20ebf6c1c395df2
noamsh <noam.shapi@gmail.com>
2024-01-25 11:28:26 +0200

    fixed merge labels added notebook for batch integration

--- data_loading/utils.py
+++ data_loading/utils.py
@@ -23,18 +23,24 @@ def load_dataframe_from_file(plates_data_path: Path) -> pd.DataFrame:
 
 
 def merge_labels_to_adata(adata: ad.AnnData, labels_df: pd.DataFrame, col_in_adata_to_merge_by: str,
-                          cols_in_labels_df_to_merge_by: str, cols_to_validate_not_empty: List[str],
-                          labels_col_names: Optional[List[str]] = None, merge_suffixes=("_x", "_y")):
-    if labels_col_names is None:
-        labels_col_names = labels_df.columns
+                          cols_in_labels_df_to_merge_by: str, cols_to_validate_not_empty: Optional[List[str]],
+                          labels_col_names_to_merge: Optional[List[str]] = None, merge_suffixes=("_x", "_y")) -> ad.AnnData:
     if cols_in_labels_df_to_merge_by == "index":
         labels_df = labels_df.reset_index()
     if col_in_adata_to_merge_by == "index":
         adata.obs.index = adata.obs.index.map(str)
     else:
         adata.obs = adata.obs.astype({col_in_adata_to_merge_by: "str"})
+
+    if labels_col_names_to_merge is None:
+        labels_col_names_to_merge = list(labels_df.columns)
+    if cols_in_labels_df_to_merge_by not in labels_col_names_to_merge:
+        labels_col_names_to_merge.append(cols_in_labels_df_to_merge_by)
+
     adata.obs = pd.merge(
         left=adata.obs.reset_index(),
-        right=labels_df[labels_col_names], how="left", validate="m:1", suffixes=merge_suffixes,
+        right=labels_df[labels_col_names_to_merge], how="left", validate="m:1", suffixes=merge_suffixes,
         left_on=col_in_adata_to_merge_by, right_on=cols_in_labels_df_to_merge_by).set_index("index")
-    adata = adata[~ adata.obs[cols_to_validate_not_empty].isna().any(axis=1), :]
+    if cols_to_validate_not_empty is not None:
+        adata = adata[~ adata.obs[cols_to_validate_not_empty].isna().any(axis=1), :].copy()
+    return adata
